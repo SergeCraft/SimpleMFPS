3e328bf47d3ec931e79df64141cca67163026276
Serge Craft <alexandrsudakov@tutanota.com>
2022-02-24 01:35:02 +0300

    Added pickup sound logic

--- Assets/Scripts/Player/MFPControllerExtensions.cs
+++ Assets/Scripts/Player/MFPControllerExtensions.cs
@@ -8,11 +8,14 @@ public class MFPControllerExtensions : MonoBehaviour
 {
 
     private SignalBus _signalBus;
+    public AudioClip _pickupSound;
+    private AudioSource _audioSource;
 
     [Inject]
     public void Construct(SignalBus signalBus)
     {
         _signalBus = signalBus;
+        _audioSource = GetComponent<AudioSource>();
     }
     
     
@@ -26,8 +29,15 @@ public class MFPControllerExtensions : MonoBehaviour
                 break;
             case "Trophy":
                 _signalBus.Fire(new TrophyPickedSignal(hit.gameObject.GetComponent<Trophy1Controller>()));
+                PlayPickupSound();
                 break;
         }
+    }
+
+    private void PlayPickupSound()
+    {
+        _audioSource.clip = _pickupSound;
+        _audioSource.Play();
     }
 
     public void OnAnimationEvent(string message)
